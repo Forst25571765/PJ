193400d634be83a1448661f8b4bccf046e9300ef
北枫 <1506037717@qq.com>
2023-12-11 08:59:56 +0800

    1

---
+++ cpu/Device.cpp
@@ -0,0 +1,35 @@
+#include <cstdint>
+#include <cstring>
+#include <functional>
+#include <queue>
+#include <stack>
+#include <string>
+
+#include "Map.h"
+#include "Pipline.h"
+#include "Device.h"
+uint8_t Char2Uint8(char c) {
+    if (c >= '0' && c <= '9') {
+        return (c - '0');
+    }
+    if (c >= 'a' && c <= 'f') {
+        return (c - 'a');
+    }
+    if (c >= 'A' && c <= 'F') {
+        return (c - 'A');
+    }
+    throw std::domain_error("code error");
+    exit(1);
+}
+Device::Device(std::string code) {
+    Stat = S_AOK;
+    int length = code.length();
+    if (length > 2 * MEM_SIZE) throw std::domain_error("code too long");
+    if (length == 0) throw std::domain_error("no code");
+    int cnt = 0;
+    auto p = code.begin();
+    while (p != code.end()) {
+        Mem[cnt++] = 16 * Char2Uint8(*p++) + Char2Uint8(*p++);
+    }
+    addr = 0;
+}
